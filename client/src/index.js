396f8c4a4031397649e4f250c3076d3ee1bbdd0a
Kbigbean <bin065025@gmail.com>
2019-11-21 15:07:05 +0900

    똥 다치우기

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import Root from "./Root";
+import './styles/base.scss';
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+
+serviceWorker.unregister();
