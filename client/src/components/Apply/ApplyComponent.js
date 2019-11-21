feeb0fd85618b224daa5245a18abb0152a01bc1c
gguggu <adrw1365@gmail.com>
2019-11-21 20:43:18 +0900

    [FIX] APPLY, REPORT
    실종자 신청 및 제보하기 수정

--- client/src/components/Apply/ApplyComponent.js
+++ client/src/components/Apply/ApplyComponent.js
@@ -1,13 +1,62 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Button } from '../common';
 
 class ApplyComponent extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      imgFile: {}
+    };
+  }
+
+handleSend = () => {
+  const { imgFile} = this.state;
+
+  const data = {
+    relation: '부',
+    name: '박건우',
+    gender: 'woman',
+    age: 18,
+    nation: '대한민국',
+    missDate: '2019/07/21',
+    missArea: '대전',
+    physicalPoint: '키가 큼',
+    additional: '보면 연락 주세요 띠링띠링',
+    binary: window.btoa(imgFile.name)
+  };
+
+  axios.post(`http://172.26.0.34:8000/missing/post/`, data, {
+    headers: {
+      'Authorization': localStorage.getItem('token')
+    }
+  }).then(res => {
+    console.log(res);
+  }).catch(err => {
+    console.error(err);
+  });
+}
+
+handleSetImg = e => {
+  console.log(e.target.files[0]);
+  this.setState({
+    imgFile: e.target.files[0]
+  })
+}
+
   render() {
     return (
       <div>
         실종자 신청
+        <input type="file" accept="image/png, image/jpeg" onChange={this.handleSetImg}/>
+        <Button onClick={this.handleSend} children={"실종자 신청하기"}/>
       </div>
     );
   }
 }
+
+// userId, relation, name, gender, age, nation, missDate, area, point, postDate,
+// profile, content
 
 export default ApplyComponent;
--- client/src/components/Report/ReportComponent.js
+++ client/src/components/Report/ReportComponent.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import { Button } from '../common';
+import axios from 'axios';
 
 class ReportComponent extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      title: '',
+      content: ''
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    console.log(e.target.name, e.target.value);
+  }
+
+  handleSend = () => {
+    const { title, content } = this.state;
+    const data = {
+      title: title,
+      content: content
+    }
+    axios.post('172.26.0.34:8080/', data, {
+      headers: {
+        'x-access-token': localStorage.getItem('token')
+      }
+    }).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.error(err);
+    });
+  }
+
   render() {
     return (
-      <input placeholder="아 겁나 피곤해" />
+      <div>
+        <input placeholder="제목입력" name="title" onChange={this.handleChange}/>
+        <input placeholder="내용입력" name="content" onChange={this.handleChange}/>
+        <Button onClick={this.handleSend} children={"제보하기"}/>
+      </div>
     );
   }
 }
