ae976a3bf11eb5c81a630d6b415303769374ec21
sema0710 <pandati@naver.com>
2019-11-22 12:26:21 +0900

    [FIX]홍보하기 기능 제작

--- client/src/components/list/MissPost/MissPost.js
+++ client/src/components/list/MissPost/MissPost.js
@@ -3,6 +3,7 @@ import React from 'react';
 import styles from './MissPost.scss';
 import classNames from 'classnames/bind';
 import {Button} from "../../common";
+import axios from 'axios'
 
 const cx = classNames.bind(styles);
 
@@ -12,7 +13,7 @@ const MissPost = ({children}) => {
     // const onClickPromotion = () => {
     //     setqewr(true);
     // };
-
+    console.log(children)
     return (
         <div className={cx('miss-post ')}>
             <div className={cx('wrap col-8')}>
@@ -21,14 +22,26 @@ const MissPost = ({children}) => {
                         <div className={cx('image col-3')}>
                             <img src={children.binary} alt=""/>
                         </div>
-                        <Button to={children.id}>홍보하기</Button>
+                        <Button to={children.id} onClick={()=>{
+                            axios.post(`http://172.26.0.34:8000/missing/promotion/${children.id}/`, {}, {
+                                headers: {
+                                    'Authorization': localStorage.getItem('access_token')
+                                } 
+                            })
+                            .then(()=>{
+                                alert("홍보하기에 성공하였습니다.");
+                            })
+                            .catch((e)=>{
+                                console.log(e);
+                            })
+                        }}>홍보하기</Button>
                     </div>
                     <div className={cx('missing col-12')}>
                         <div className={cx('miss-head col-12')}>
                             <h3>{children.name}</h3><h3>({children.gender})</h3>
                         </div>
                         <div className={cx('missing-data col-12')}>
-                            <p>당시나이 4세</p><p>현재나이 {children.age}</p>
+                            <p>현재나이 {children.age}</p>
                         </div>
                         <div className={cx('missing-data col-12')}>
                             <p>국적 {children.nation}</p><p>발생장소 {children.missArea}</p>
