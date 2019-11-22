cb7a4a459111d14563f89dbb8cdb57b35f51e875
sema0710 <pandati@naver.com>
2019-11-22 09:58:02 +0900

    [fix]header token 유무에 따라서 변경되게 함

--- client/src/components/common/Header/Header.js
+++ client/src/components/common/Header/Header.js
@@ -8,6 +8,14 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 const Header = () => {
+    let token = localStorage.getItem("access_token");
+    let isLogin = [];
+    if(token === ""){
+        isLogin.push(<li><Link to="/register" className={cx('link')}>회원가입</Link></li>);
+        isLogin.push(<li><Link to="/login" className={cx('link')}>로그인</Link></li>)
+    } else{
+        isLogin.push(<li><Link to="/" onClick={()=>{localStorage.setItem("access_token","")}}className={cx('link')}>로그아웃</Link></li>)
+    }
     return (
         <div className={cx('header col-12')}>
             <nav className={cx('container')}>
@@ -19,9 +27,7 @@ const Header = () => {
                     <li><Link to="/apply" className={cx('link')}>실종자 신청</Link></li>
                     <li><Link to="/report" className={cx('link')}>제보하기</Link></li>
                     <li><Link to="/mypage" className={cx('link')}>내정보</Link></li>
-                    <li><Link to="/로그아웃" className={cx('link')}>로그아웃</Link></li>
-                    {/*<li><Link to="/회원가입" className={cx('link')}>회원가입</Link></li>*/}
-                    {/*<li><Link to="/로그인" className={cx('link')}>로그인</Link></li>*/}
+                    {isLogin}
                 </ul>
             </nav>
         </div>
