40759fa0380ac18339e9d0f846b18b03e45d4e11
sema0710 <pandati@naver.com>
2019-11-22 12:39:04 +0900

    footer 업뎃

--- client/src/components/common/Footer/Footer.js
+++ client/src/components/common/Footer/Footer.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from '../../../images/MainPage/rewind-logo.png'
 import './Footer.scss';
+import axios from 'axios'
+
 
 const Footer = () => {
+
+    const getUser = ()=>{
+        console.log("hello!");
+        axios({
+            method:"GET",
+            url:"http://172.26.0.34:8000/missing/promotion/"
+        })
+        .then((e)=>{
+            console.log(e);
+        })
+    }
+
+    useEffect(()=>{
+        getUser();
+    },[])
     return (
             <div className="App-section">
                 <div className="sponsor">
                     <div className="sponsor-wrapper">
-                        <div className="sponsor-image"><img src={logo} className="App-logo" alt="logo"/></div>
-                        <div className="sponsor-image"><img src={logo} className="App-logo" alt="logo"/></div>
                         <div className="sponsor-image"><img src={logo} className="App-logo" alt="logo"/></div>
                         <div className="sponsor-image"><img src={logo} className="App-logo" alt="logo"/></div>
                         <div className="sponsor-image"><img src={logo} className="App-logo" alt="logo"/></div>
