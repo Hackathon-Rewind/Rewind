75241936dc9fe4873e0031b0dcfadd76608c60ed
Unperknown <wnsdud105@gmail.com>
2019-11-22 12:41:53 +0900

    [Updated] Banner

--- client/src/components/Main/Banner/Banner.js
+++ client/src/components/Main/Banner/Banner.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import banner from '../../../images/MainPage/banner.jpg';
+import banner1 from '../../../images/background4.jpg';
+import banner2 from '../../../images/background3.jpg';
+import banner3 from '../../../images/background1.jpg';
 import './Banner.scss';
 
 const Banner = () => {
@@ -9,9 +11,18 @@ const Banner = () => {
             <span id="slider-image-2"></span>
             <span id="slider-image-3"></span>
             <div className="image-holder">
-                <img src={banner} alt="" className="slider-image" />
-                <img src={banner} alt="" className="slider-image" />
-                <img src={banner} alt="" className="slider-image" />
+                <div className="slider-image">
+                    <img src={banner1} alt="" />
+                    <span>당신의 작은 관심, 한 가정을 지킵니다.</span>
+                </div>
+                <div className="slider-image">
+                    <img src={banner2} alt="" />
+                    <span>길을 잃어버린 아이에게 집을 찾아줄 수 있어요.</span>
+                </div>
+                <div className="slider-image">
+                    <img src={banner3} alt="" />
+                    <span>누군가의 소중한 사람, 당신도 찾을 수 있습니다.</span>
+                </div>
             </div>
             <div className="button-holder">
                 <a href="#slider-image-1"><span className="slider-change"></span></a>
